51498fcd31f691a10ec302021eb30f0cfca731e5
gangkui <gangkui1688@gmail.com>
2019-08-01 11:39:45 +0700

    finish the core  working

--- admin/ctl/ctl_member.php
+++ admin/ctl/ctl_member.php
@@ -1,5 +1,16 @@
 <?php
-namespace admin/ctl;
+namespace admin\ctl;
+use sephp\sephp;
+use sephp\core\req;
+use sephp\core\log;
+use sephp\core\view;
+use sephp\lib\power;
+use sephp\core\pages;
+use sephp\core\db;
+use sephp\core\upload;
+use sephp\core\show_msg;
+use sephp\core\session;
+use sephp\core\config;
 
 /**
  * 会员管理
